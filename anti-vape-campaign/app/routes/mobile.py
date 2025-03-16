de29725861cdc2833aa91629406fad53a2fc9abd
sirivimol-prae <sirivimol.prae@gmail.com>
2025-03-16 19:08:21 +0700

    update mobile

--- anti-vape-campaign/app/routes/mobile.py
+++ anti-vape-campaign/app/routes/mobile.py
@@ -104,7 +104,9 @@ def page5_2_2():
 
 @mobile_bp.route('/case2/page5_2_3')
 def page5_2_3():
-    return render_template('case2/page5_2_3.html')
+    result = supabase.from_('guardians').select('selected_flower').eq('id', session['user_id']).execute()
+    selected_flower = result.data[0]['selected_flower'] if result.data else 1
+    return render_template('case2/page5_2_3.html', selected_flower=selected_flower)
 
 @mobile_bp.route('/case2/page5_2_4')
 def page5_2_4():
